239a327f852c22bc9013d1f2209a2096572ef37a
Everton da Rosa <everton3x@gmail.com>
2024-06-04 10:06:38 -0300

    close #19

--- resources/views/app/partials/form/field/despesa.blade.php
+++ resources/views/app/partials/form/field/despesa.blade.php
@@ -1,6 +1,6 @@
 <div class="required field">
     <label for="despesa">Despesa</label>
-    <input type="text" name="despesa" id="despesa" required value="{{ $value }}" class="enter-as-tab" pattern="[0-9]{6}" title="Apenas números, com 6 dígitos, como em 339030">
+    <input type="text" name="despesa" id="despesa" required value="{{ $value }}" class="enter-as-tab" pattern="[0-9]{6}" title="Apenas números, com 6 dígitos, como em 339030" maxlength="6">
     @error('despesa')
         @include('app.partials.form.error-message', ['message' => $message])
     @enderror
--- resources/views/app/partials/form/field/fonte.blade.php
+++ resources/views/app/partials/form/field/fonte.blade.php
@@ -1,6 +1,6 @@
 <div class="required field">
     <label for="fonte">Fonte</label>
-    <input type="text" name="fonte" id="fonte" required value="{{ $value }}" class="enter-as-tab" pattern="[0-9]{5}" title="Apenas números, com 5 dígitos, como em 62101">
+    <input type="text" name="fonte" id="fonte" required value="{{ $value }}" class="enter-as-tab" pattern="[0-9]{5}" title="Apenas números, com 5 dígitos, como em 62101" maxlength="5">
     @error('fonte')
         @include('app.partials.form.error-message', ['message' => $message])
     @enderror
